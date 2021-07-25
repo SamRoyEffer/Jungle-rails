403ae2741e3552ce19c72983dedd32ca3329b6ac
Sam Roy-Effer <samroyeffer@gmail.com>
2021-07-25 14:47:34 -0400

    add categories to admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,10 @@
 class Admin::CategoriesController < ApplicationController
 
+ http_basic_authenticate_with name:
+ ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
   def index
+    @categories = Category.order(name: :asc).all
   end
 
   def new
