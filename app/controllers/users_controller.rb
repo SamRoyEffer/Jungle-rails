e0974790cd9efe0b26aa8caa6f72b4d80fd5452d
Sam Roy-Effer <samroyeffer@gmail.com>
2021-07-26 15:36:50 -0400

    fix database file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,15 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
-     render :new
+     render "new"
     end
   end
   private
 
   def user_params
     params.require(:user).permit(
-      :name,
+      :first_name,
+      :last_name,
       :email,
       :password,
       :password_confirmation
