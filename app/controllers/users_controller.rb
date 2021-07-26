365b4e963315a841bc7543c84b9b4b834aad5b00
Sam Roy-Effer <samroyeffer@gmail.com>
2021-07-26 16:18:46 -0400

    fix signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to('/')
     else
      render "new"
     end
