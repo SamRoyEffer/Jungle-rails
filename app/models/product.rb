15e4611f98f632fa1a6671f97f5e83b6df66881a
Sam Roy-Effer <samroyeffer@gmail.com>
2021-07-27 15:37:54 -0400

    create test cases for authtication and users

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,12 @@
 class Product < ActiveRecord::Base
-
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
-
   belongs_to :category
-
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
-
+  def sold_out?
+    self.quantity == 0
+  end
 end
